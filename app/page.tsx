847424ed03e68e02bc47db32ff5136c7feff9dbf
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-05-03 21:04:07 +0300

    - implemented about page - added suspense when loading featured product section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import LoadingContainer from '@/components/global/LoadingContainer';
 import FeaturedProducts from '@/components/home/FeaturedProducts';
 import Hero from '@/components/home/Hero';
+import { Suspense } from 'react';
 
 function HomePage() {
   return (
     <div>
       <Hero />
+      <Suspense fallback={<LoadingContainer />}>
       <FeaturedProducts/>
+      </Suspense>
     </div>
   );
 }
