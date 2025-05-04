3578cd2dfcc685d7df349ae3d892cab1a8ed4887
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-05-04 22:08:01 +0300

    added auth provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import { ThemeProvider } from '@/app/theme-provider';
+import { ToastProvider } from '@/components/ui/toast';
 
-function Providers({ children }: { children: React.ReactNode }) {
-  return <>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
-  </>;
+function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <>
+      <ToastProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </ToastProvider>
+    </>
+  )
 }
+
 export default Providers;
