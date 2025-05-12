21e4eda795c64ed759a75e017edca4d2c3d2ab73
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-05-13 00:14:08 +0300

    implemented create product page

---
+++ components/form/Buttons.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
+import { useFormStatus } from 'react-dom';
+
+type btnSize = 'default' | 'lg' | 'sm';
+
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+export function SubmitButton({className = '', text = 'submit', size = 'lg'}: SubmitButtonProps) {
+  const {pending} = useFormStatus();
+
+  return (
+    <Button
+      type='submit'
+      disabled={pending}
+      className={cn('capitalize', className)}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <Loader2 className='mr-2 h-4 w-4 animate-spin'/>
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,25 @@
-import db from './db';
+'use server'
+
+import { imageSchema, productSchema, validateWithZodSchema } from '@/utils/schema';
+import { uploadImage } from '@/utils/supabase';
+import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import db from './db';
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : 'An error occurred',
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('You must be logged in to access this route');
+  }
+  return user;
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -11,12 +31,12 @@ export const fetchFeaturedProducts = async () => {
 }
 
 
-export const fetchAllProducts = async (search: string  = '') => {
+export const fetchAllProducts = async (search: string = '') => {
   const products = await db.product.findMany({
     where: {
       OR: [
-        { name: { contains: search, mode: 'insensitive' } },
-        { company: { contains: search, mode: 'insensitive' } },
+        {name: {contains: search, mode: 'insensitive'}},
+        {company: {contains: search, mode: 'insensitive'}},
       ],
     },
     orderBy: {
@@ -37,3 +57,27 @@ export const fetchSingleProduct = async (productId: string) => {
   }
   return product;
 };
+
+export const createProductAction = async (prevState: any, formData: FormData) => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const file = formData.get('image') as File;
+    const validatedFields = validateWithZodSchema(productSchema, rawData);
+    const validatedFile = validateWithZodSchema(imageSchema, { image: file });
+    const fullPath = await uploadImage(validatedFile.image);
+
+    await db.product.create({
+      data: {
+        ...validatedFields,
+        image: fullPath,
+        clerkId: user.id,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+
+  redirect('/admin/products');
+}
