fd29bc2f71d7d74fde19ea35e183dbe509a4d8c8
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-05-15 15:06:54 +0300

    implemented favorite product

--- components/products/FavoriteToggleForm.tsx
+++ components/products/FavoriteToggleForm.tsx
@@ -1,8 +1,29 @@
-function FavoriteToggleForm() {
+'use client';
+
+import { toggleFavoriteAction } from '@/utils/actions';
+import { usePathname } from 'next/navigation';
+import { CardSubmitButton } from '../form/Buttons';
+import FormContainer from '../form/FormContainer';
+
+type FavoriteToggleFormProps = {
+  productId: string;
+  favoriteId: string | null;
+};
+
+function FavoriteToggleForm({
+                              productId,
+                              favoriteId,
+                            }: FavoriteToggleFormProps) {
+  const pathname = usePathname();
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    productId,
+    favoriteId,
+    pathname,
+  });
   return (
-    <div>
-      FavoriteToggleForm
-    </div>
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false}/>
+    </FormContainer>
   );
 }
 
