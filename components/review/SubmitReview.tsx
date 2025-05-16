5f00d88eab1b84eb19dda3e780071b0d85162faa
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-05-16 12:52:44 +0300

    added product review feature

---
+++ components/review/SubmitReview.tsx
@@ -0,0 +1,54 @@
+// todo check if we can make this component server side rendered
+
+'use client';
+import { SubmitButton } from '@/components/form/Buttons';
+import FormContainer from '@/components/form/FormContainer';
+import TextAreaInput from '@/components/form/TextAreaInput';
+import RatingInput from '@/components/review/RatingInput';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { createReviewAction } from '@/utils/actions';
+import { useUser } from '@clerk/nextjs';
+import { useState } from 'react';
+
+function SubmitReview({productId}: { productId: string }) {
+  const [isReviewFormVisible, setIsReviewFormVisible] = useState(false);
+  const {user} = useUser();
+  return (
+    <div>
+      <Button
+        size='lg'
+        className='capitalize'
+        onClick={() => setIsReviewFormVisible((prev) => !prev)}
+      >
+        leave review
+      </Button>
+      {isReviewFormVisible && (
+        <Card className='p-8 mt-8'>
+          <FormContainer action={createReviewAction}>
+            <input type='hidden' name='productId' value={productId}/>
+            <input
+              type='hidden'
+              name='authorName'
+              value={user?.firstName || 'user'}
+            />
+            <input
+              type='hidden'
+              name='authorImageUrl'
+              value={user?.imageUrl || ''}
+            />
+            <RatingInput name='rating'/>
+            <TextAreaInput
+              name='comment'
+              labelText='feedback'
+              defaultValue='Outstanding product!!!'
+            />
+            <SubmitButton className='mt-4'/>
+          </FormContainer>
+        </Card>
+      )}
+    </div>
+  );
+}
+
+export default SubmitReview;
--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,18 +1,1 @@
-import { FaStar } from 'react-icons/fa';
-
-async function ProductRating({ productId }: { productId: string }) {
-  const rating = 4.2;
-  const count = 25;
-
-  const className = `flex gap-1 items-center text-md mt-1 mb-4`;
-  const countValue = `(${count}) reviews`;
-  console.log('ProductRating', productId);
-  return (
-    <span className={className}>
-      <FaStar className='w-3 h-3' />
-      {rating} {countValue}
-    </span>
-  );
-}
-
-export default ProductRating;
+import { fetchProductRating } from '@/utils/actions';import { FaStar } from 'react-icons/fa';async function ProductRating({productId}: { productId: string }) {  const {rating, count} = await fetchProductRating(productId);  const className = `flex gap-1 items-center text-md mt-1 mb-4`;  const countValue = `(${count}) reviews`;  return (    <span className={className}>      <FaStar className='w-3 h-3'/>      {rating} {countValue}    </span>  );}export default ProductRating;
