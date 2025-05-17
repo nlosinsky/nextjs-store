8bda489d9791cac66cbc32e57d699e702bd5dd4d
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-05-17 11:55:48 +0300

    implemented add to cart functionality

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { fetchCartItems } from '@/utils/actions';
 import Link from 'next/link';
 import { LuShoppingCart } from 'react-icons/lu';
 
-function CartButton() {
-  const numItemsInCart = 9;
+async function CartButton() {
+  const numItemsInCart = await fetchCartItems();
+
   return (
     <Button asChild
             size='icon'
@@ -11,8 +13,9 @@ function CartButton() {
             className="relative"
     >
       <Link href="/cart">
-        <LuShoppingCart />
-        <span className="absolute -top-3 -right-3 rounded-full bg-primary w-6 h-6 text-white inline-flex items-center justify-center text-xs">
+        <LuShoppingCart/>
+        <span
+          className="absolute -top-3 -right-3 rounded-full bg-primary w-6 h-6 text-white inline-flex items-center justify-center text-xs">
           {numItemsInCart}
         </span>
       </Link>
