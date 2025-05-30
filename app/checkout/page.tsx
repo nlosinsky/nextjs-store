56005d734554b17aeff04a41b3f535560eb588a4
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-05-30 23:54:22 +0300

    integrated stripe

---
+++ app/checkout/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+import { EmbeddedCheckout, EmbeddedCheckoutProvider, } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import React, { useCallback } from 'react';
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
+
+function CheckoutPage() {
+  const searchParams = useSearchParams();
+
+  const orderId = searchParams.get('orderId');
+  const cartId = searchParams.get('cartId');
+
+  const fetchClientSecret = useCallback(async () => {
+    // Create a Checkout Session
+    const response = await axios.post('/api/payment', {
+      orderId: orderId,
+      cartId: cartId,
+    });
+
+    return response.data.clientSecret;
+  }, []);
+
+  const options = {fetchClientSecret};
+
+  return (
+    <div id='checkout'>
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout/>
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
+
+export default CheckoutPage;
